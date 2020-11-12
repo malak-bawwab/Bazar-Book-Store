1ffeabfdc1631d1883554ad637674b6702473181
Catalog Server <malakbawab29@gmail.com>
2020-11-11 23:19:33 -0800

    Define controller method to handle search route

---
+++ BazarCatalog/app/Http/Controllers/BooksController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+
+class BooksController extends Controller
+{
+
+
+    public function showBasedOnTopic($topic)
+    {
+$topic = str_replace('%20',' ',$topic);
+$result= DB::select('select * from books where topic=?',[$topic]);
+
+if(!empty($result)){
+
+return response()->json(['Books'=>$result]);
+   
+              
+  }
+  return response()->json(['message'=>'There is no book with this topic']);
+  
+  }}
