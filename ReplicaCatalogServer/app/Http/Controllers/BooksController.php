3642cdd0e4e92093ec713d52325edf040de8693a
root <malakbawab29@gmail.com>
2020-12-16 10:34:42 -0800

    Code for replica catalog,is the same as catalog but there is some changes in IPs

---
+++ ReplicaCatalogServer/app/Http/Controllers/BooksController.php
@@ -0,0 +1,180 @@
+<?php
+namespace App\Http\Controllers;
+use GuzzleHttp\Client;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+
+class BooksController extends Controller
+{
+
+
+
+public function updateAndNotify($itemNumber,$type,$value){
+$res=$this->updateStoreQuantity($itemNumber,$type,$value);
+if(strpos($res,"Not Found") || strpos($res,"You only have")){
+}else{
+
+ $this->sendUpdateNotification($itemNumber,$type,$value);
+}
+
+return $res;
+}
+
+
+public function updateCostAndNotify($itemNumber,$newCost){
+$res=$this->updateCost($itemNumber,$newCost);
+if(strpos($res,"Not Found")){
+}else{
+
+ $this->sendUpdateNotification($itemNumber,"cost",$newCost);
+}
+
+return $res;
+}
+
+//search for a book in books table based on topic
+    public function showBasedOnTopic($topic)
+    {
+//covert %20 to spaces
+$topic = str_replace('%20',' ',$topic);
+$result= DB::select('select * from books where topic=?',[$topic]);
+
+if(!empty($result)){
+
+return response()->json($result);
+   
+              
+  }
+  return response()->json('Try again,There is no book with this topic'.' '.$topic);
+  
+  }
+  //search for a book in books table based on the itemNumber
+    public function showBasedOnItemNumber($itemNumber)
+    {
+
+$result= DB::select('select * from books where id=?',[$itemNumber]);
+
+if(!empty($result)){
+
+return response()->json($result);
+   
+              
+  }
+  return response()->json('Try again,There is no book with this itemNumber'.' '.$itemNumber);
+  
+  }
+/* check
+if the book exists or not,out of stock or not,it is called by order service
+through Guzzle http client in buy operation.
+*/
+    public function checkIfExists($itemNumber)
+    {
+    
+    $result1= DB::select('select * from books where id=?',[$itemNumber]);
+ 
+    $result2=DB::select('select * from books where id=? and quantity>0',[$itemNumber]);
+
+ if(!empty($result1) && !empty($result2)){
+   return response()->json(['message'=>'Found,Not out of stock']);
+ 
+ }elseif(!empty($result1) && empty($result2)){
+    return response()->json(['message'=>'Found  but out of stock']);
+ }
+ elseif(empty($result1)){
+     return response()->json(['message'=>'Not Found']);
+ }
+
+  
+  }
+public function  updateStoreQuantity($itemNumber,$type,$value){
+$result1= DB::select('select * from books where id=?',[$itemNumber]);
+if($type=="buy"){
+$quantity=$result1[0]->quantity-1;
+if($quantity<0){
+$quantity=0;
+}
+$result2=DB::update('update books set quantity = '  .$quantity. ' where id= ?' ,[$itemNumber]);
+     $this->sendInvalidateRequest($itemNumber,$result1[0]->topic);
+
+
+return response()->json(['message'=>'Bought book'.' '.$result1[0]->title]);
+
+}else if(!empty($result1)){
+   if ($type=="increaseNumber" ){
+    
+$quantity=$result1[0]->quantity+$value;
+}else if($type=="decreaseNumber"){
+$quantity=$result1[0]->quantity-$value;
+
+}else if($type=="newValue"){
+
+$quantity=$value;
+
+}
+}else {
+return response()->json(['message'=>"Book(with this itemNumber".$itemNumber.')'." Not Found"]);
+}
+
+
+if($quantity<0){
+return response()->json(['message'=>"You only have".' '.$result1[0]->quantity]);
+
+}else{
+$result2=DB::update('update books set quantity = '  .$quantity. ' where id= ?' ,[$itemNumber]);
+    $this->sendInvalidateRequest($itemNumber,$result1[0]->topic);
+ return response()->json(['message'=>"Book".'('.$result1[0]->title.')'."Quantity is updated Successfully from".$result1[0]->quantity." to ".$quantity]);
+
+}
+
+}
+   
+  public function  updateCost($itemNumber,$newCost)    {
+    $result= DB::select('select * from books where id=?',[$itemNumber]);
+
+if(!empty($result)){
+
+
+    $result1=DB::update('update books set price = '  .$newCost. ' where id= ?' ,[$itemNumber]);
+ $this->sendInvalidateRequest($itemNumber,$result1[0]->topic);
+  return response()->json(['message'=>"Book".'('.$result[0]->title.')'."Cost is updated Successfully"." From".' '.$result[0]->price.' '."To ".$newCost]);
+   }else{
+
+
+return response()->json(['message'=>"Book(with this itemNumber".$itemNumber.')'." Not Found"]);
+
+}
+} 
+public function sendUpdateNotification($itemNumber,$updateType,$newValue){
+  
+ $client = new Client();
+$notifyRequest='http://192.168.164.129/notify/'.$itemNumber.'/'.$updateType.'/'.$newValue;
+ $res1= $client->request('GET',  $notifyRequest);
+   
+    return $res1->getStatusCode();
+}
+
+public function sendInvalidateRequest($itemNumber,$topic){
+    $client = new Client();
+$invalidateRequest='http://192.168.164.128/invalidate/'.$itemNumber.'/'.$topic;
+ $res1= $client->request('GET',  $invalidateRequest);
+   
+        return $res1->getStatusCode();
+
+}
+
+public function applyUpdates($itemNumber,$updateType,$newValue){
+
+if($updateType=="cost"){
+$this->updateCost($itemNumber,$newValue);
+}else {
+
+ $this->updateStoreQuantity($itemNumber,$updateType,$newValue);
+}
+
+}
+  
+}
---
+++ ReplicaCatalogServer/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+
+
+$router->get('/search/{topic}', ['uses' => 'BooksController@showBasedOnTopic']);
+$router->get('/lookup/{itemNumber}', ['uses' => 'BooksController@showBasedOnItemNumber']);
+
+
+$router->get('/notify/{itemNumber}/{updateType}/{newValue}', ['uses' => 'BooksController@applyUpdates']);
+
+// to complete buy process,will be called from order service
+$router->get('/query/{itemNumber}', ['uses' => 'BooksController@checkIfExists']);
+// to complete buy process,will be called from order service
+$router->put('/update/book/{itemNumber}/type/{type}/value/{value}', ['uses' => 'BooksController@updateAndNotify']);
+
+$router->put('/update/book/{itemNumber}/cost/{newCost}', ['uses' => 'BooksController@updateCostAndNotify']);
+
